c6c44c6f88eb676444df8b174e4747a435aa4afd
xupnge <xupnge@yahoo.com>
2021-12-02 14:57:07 +0800

    增加get方法

--- src/live/AlibabaCloudLiveClient.php
+++ src/live/AlibabaCloudLiveClient.php
@@ -14,11 +14,19 @@ class AlibabaCloudLiveClient extends AlibabaCloudClient {
     private  $version = '2016-11-01';
     private  $host = 'live.aliyuncs.com';
 
-    function getProduct()
+    protected function getProduct()
     {
         return 'live';
     }
 
+    /**
+     * 通过 post方法 请求API
+     * @param $action
+     * @param $options
+     * @return array
+     * @throws \AlibabaCloud\Client\Exception\ClientException
+     * @throws \AlibabaCloud\Client\Exception\ServerException
+     */
     public function requestByPost($action,$options){
         $this->setDefaultClient();
         $result = AlibabaCloud::rpc()
@@ -26,6 +34,27 @@ class AlibabaCloudLiveClient extends AlibabaCloudClient {
             ->version($this->version)
             ->action($action)
             ->method('POST')
+            ->host($this->host)
+            ->options(['query' => $options])
+            ->request();
+        return $result->toArray();
+    }
+
+    /**
+     * 通过 get方法 请求API
+     * @param $action
+     * @param $options
+     * @return array
+     * @throws \AlibabaCloud\Client\Exception\ClientException
+     * @throws \AlibabaCloud\Client\Exception\ServerException
+     */
+    public function requestByGet($action,$options){
+        $this->setDefaultClient();
+        $result = AlibabaCloud::rpc()
+            ->product($this->getProduct())
+            ->version($this->version)
+            ->action($action)
+            ->method('GET')
             ->host($this->host)
             ->options(['query' => $options])
             ->request();
